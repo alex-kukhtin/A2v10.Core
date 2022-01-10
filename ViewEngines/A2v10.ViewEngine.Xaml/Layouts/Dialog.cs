59a5284aa2a634e64e263b63f9c129f83fe294dd
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-01-10 13:52:07 +0200

    save event

--- ViewEngines/A2v10.ViewEngine.Xaml/Layouts/Dialog.cs
+++ ViewEngines/A2v10.ViewEngine.Xaml/Layouts/Dialog.cs
@@ -1,252 +1,253 @@
-﻿// Copyright © 2015-2021 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2015-2022 Alex Kukhtin. All rights reserved.
 
-using System;
 using System.Text;
 using A2v10.Infrastructure;
 
-namespace A2v10.Xaml
+namespace A2v10.Xaml;
+public enum DialogSize
 {
-	public enum DialogSize
+	Default = 0,
+	Small = 1,
+	Medium = Default,
+	Large = 2,
+	Max = 3
+}
+
+public class Dialog : RootContainer, ISupportTwoPhaseRendering
+{
+	public String? Title { get; set; }
+	public String? HelpUrl { get; set; }
+	public String? TestId { get; set; }
+	public String? SaveEvent { get; set; }
+
+	public UIElementBase? TitleInfo { get; set; }
+
+	public DialogSize Size { get; set; }
+	public Length? Width { get; set; }
+	public Length? MinWidth { get; set; }
+	public Length? Height { get; set; }
+	public String? CanCloseDelegate { get; set; }
+	public Boolean AlwaysOk { get; set; }
+	public UIElementBase? Taskpad { get; set; }
+	public Boolean ShowWaitCursor { get; set; }
+	public BackgroundStyle Background { get; set; }
+	public Boolean Maximize { get; set; }
+	public Boolean ButtonOnTop { get; set; }
+	public Boolean Overflow { get; set; }
+
+	public UIElementCollection Buttons { get; set; } = new UIElementCollection();
+
+	protected virtual void OnCreateContent(TagBuilder tag)
 	{
-		Default = 0,
-		Small = 1,
-		Medium = Default,
-		Large = 2,
-		Max = 3
 	}
 
-	public class Dialog : RootContainer, ISupportTwoPhaseRendering
+	internal Boolean IsContentIsIFrame => Children?.Count == 1 && Children[0] is IFrame;
+
+	String? GetControllerAttributes()
 	{
-		public String? Title { get; set; }
-		public String? HelpUrl { get; set; }
-		public String? TestId { get; set; }
+		if (String.IsNullOrEmpty(CanCloseDelegate) && !AlwaysOk && String.IsNullOrEmpty(SaveEvent))
+			return null;
+		var opts = new StringBuilder("{");
+		if (!String.IsNullOrEmpty(CanCloseDelegate))
+			opts.Append($"'canClose': '{CanCloseDelegate}',");
+		if (AlwaysOk)
+			opts.Append("'alwaysOk': true,");
+		if (!String.IsNullOrEmpty(SaveEvent))
+			opts.Append($"'saveEvent': '{SaveEvent}',");
+		opts.RemoveTailComma();
+		opts.Append('}');
+		return opts.ToString();
+	}
 
-		public UIElementBase? TitleInfo { get; set; }
+	public override void RenderElement(RenderContext context, Action<TagBuilder>? onRender = null)
+	{
+		var dialog = new TagBuilder("div", "modal");
+		dialog.MergeAttribute("id", context.RootId);
+		dialog.MergeAttribute("v-cloak", String.Empty);
+		dialog.AddCssClassBoolNo(UserSelect, "user-select");
+		dialog.MergeAttribute("data-controller-attr", GetControllerAttributes());
+		dialog.AddCssClassBool(Maximize, "maximize");
+		dialog.AddCssClassBool(ButtonOnTop, "button-on-top");
 
-		public DialogSize Size { get; set; }
-		public Length? Width { get; set; }
-		public Length? MinWidth { get; set; }
-		public Length? Height { get; set; }
-		public String? CanCloseDelegate { get; set; }
-		public Boolean AlwaysOk { get; set; }
-		public UIElementBase? Taskpad { get; set; }
-		public Boolean ShowWaitCursor { get; set; }
-		public BackgroundStyle Background { get; set; }
-		public Boolean Maximize { get; set; }
-		public Boolean ButtonOnTop { get; set; }
-		public Boolean Overflow { get; set; }
+		if (!String.IsNullOrEmpty(TestId) && context.IsDebugConfiguration)
+			dialog.MergeAttribute("test-id", TestId);
 
-		public UIElementCollection Buttons { get; set; } = new UIElementCollection();
 
-		protected virtual void OnCreateContent(TagBuilder tag)
+		if (Maximize)
+			dialog.MergeAttribute("v-maximize", "true");
+		else
+			SetSize(dialog);
+
+		dialog.RenderStart(context);
+
+		if (ShowWaitCursor)
+			new TagBuilder("wait-cursor", "dialog")
+				.MergeAttribute(":ready", "$data.$ready")
+				.Render(context, TagRenderMode.Normal);
+
+
+		RenderHeader(context);
+		RenderLoadIndicator(context);
+
+
+		var content = new TagBuilder("div", "modal-content");
+		OnCreateContent(content);
+		if (Height != null)
+			content.MergeStyle("min-height", Height.Value);
+		if (Padding != null)
+			Padding.MergeStyles("padding", content);
+		content.AddCssClassBool(IsContentIsIFrame, "content-iframe"); // bug fix (3px height)
+		if (Background != BackgroundStyle.Default)
+			content.AddCssClass("background-" + Background.ToString().ToKebabCase());
+		content.AddCssClassBool(Overflow, "overflow");
+		content.RenderStart(context);
+		if (Taskpad != null)
 		{
+			var grid = new TagBuilder("div", "dialog-grid");
+			if (Taskpad is Taskpad tp && tp.Width != null)
+				grid.MergeStyle("grid-template-columns", $"1fr {tp.Width.Value}");
+			grid.RenderStart(context);
+			var gridContent = new TagBuilder("div", "dialog-grid-content");
+			gridContent.RenderStart(context);
+			RenderChildren(context);
+			gridContent.RenderEnd(context);
+			Taskpad.RenderElement(context);
+			grid.RenderEnd(context);
+		}
+		else
+		{
+			RenderChildren(context);
 		}
 
-		internal Boolean IsContentIsIFrame => Children?.Count == 1 && Children[0] is IFrame;
+		content.RenderEnd(context);
 
-		String? GetControllerAttributes()
+		RenderFooter(context);
+		RenderAccelCommands(context);
+		RenderContextMenus();
+
+		dialog.RenderEnd(context);
+	}
+
+	public override void RenderChildren(RenderContext context, Action<TagBuilder>? onRenderStatic = null)
+	{
+		// static without wrapper
+		foreach (var c in Children)
+			c.RenderElement(context);
+	}
+
+
+	void SetSize(TagBuilder dialog)
+	{
+		if ((Size == DialogSize.Default || Size == DialogSize.Medium) && (Width == null))
+			return;
+		var sb = new StringBuilder("{");
+		if (Size == DialogSize.Large)
+			sb.Append("cssClass:'modal-large',");
+		else if (Size == DialogSize.Small)
+			sb.Append("cssClass:'modal-small',");
+		if (Width != null)
+			sb.Append($"width:'{Width.Value}',");
+		if (MinWidth != null)
+			sb.Append($"minWidth:'{MinWidth.Value}',");
+		sb.RemoveTailComma();
+		sb.Append('}');
+		dialog.MergeAttribute("v-modal-width", sb.ToString());
+	}
+
+	void RenderHeader(RenderContext context)
+	{
+		var header = new TagBuilder("div", "modal-header");
+		if (!Maximize)
+			header.MergeAttribute("v-drag-window", String.Empty);
+		header.RenderStart(context);
+		var hdr = GetBinding(nameof(Title));
+		if ((hdr != null) || (Title != null))
 		{
-			if (String.IsNullOrEmpty(CanCloseDelegate) && !AlwaysOk)
-				return null;
-			var opts = new StringBuilder("{");
-			if (!String.IsNullOrEmpty(CanCloseDelegate))
-				opts.Append($"'canClose': '{CanCloseDelegate}',");
-			if (AlwaysOk)
-				opts.Append("'alwaysOk': true,");
-			opts.RemoveTailComma();
-			opts.Append('}');
-			return opts.ToString();
+			var span = new TagBuilder("span", "modal-title");
+			if (hdr != null)
+				span.MergeAttribute("v-text", hdr.GetPathFormat(context));
+			else if (Title != null)
+				span.SetInnerText(context.LocalizeCheckApostrophe(Title));
+			span.Render(context);
 		}
-
-		public override void RenderElement(RenderContext context, Action<TagBuilder>? onRender = null)
+		if (TitleInfo != null)
 		{
-			var dialog = new TagBuilder("div", "modal");
-			dialog.MergeAttribute("id", context.RootId);
-			dialog.MergeAttribute("v-cloak", String.Empty);
-			dialog.AddCssClassBoolNo(UserSelect, "user-select");
-			dialog.MergeAttribute("data-controller-attr", GetControllerAttributes());
-			dialog.AddCssClassBool(Maximize, "maximize");
-			dialog.AddCssClassBool(ButtonOnTop, "button-on-top");
-
-			if (!String.IsNullOrEmpty(TestId) && context.IsDebugConfiguration)
-				dialog.MergeAttribute("test-id", TestId);
-
-
-			if (Maximize)
-				dialog.MergeAttribute("v-maximize", "true");
-			else
-				SetSize(dialog);
-
-			dialog.RenderStart(context);
-
-			if (ShowWaitCursor)
-				new TagBuilder("wait-cursor", "dialog")
-					.MergeAttribute(":ready", "$data.$ready")
-					.Render(context, TagRenderMode.Normal);
-
-
-			RenderHeader(context);
-			RenderLoadIndicator(context);
-
-
-			var content = new TagBuilder("div", "modal-content");
-			OnCreateContent(content);
-			if (Height != null)
-				content.MergeStyle("min-height", Height.Value);
-			if (Padding != null)
-				Padding.MergeStyles("padding", content);
-			content.AddCssClassBool(IsContentIsIFrame, "content-iframe"); // bug fix (3px height)
-			if (Background != BackgroundStyle.Default)
-				content.AddCssClass("background-" + Background.ToString().ToKebabCase());
-			content.AddCssClassBool(Overflow, "overflow");
-			content.RenderStart(context);
-			if (Taskpad != null)
-			{
-				var grid = new TagBuilder("div", "dialog-grid");
-				if (Taskpad is Taskpad tp && tp.Width != null)
-					grid.MergeStyle("grid-template-columns", $"1fr {tp.Width.Value}");
-				grid.RenderStart(context);
-				var gridContent = new TagBuilder("div", "dialog-grid-content");
-				gridContent.RenderStart(context);
-				RenderChildren(context);
-				gridContent.RenderEnd(context);
-				Taskpad.RenderElement(context);
-				grid.RenderEnd(context);
-			}
-			else
-			{
-				RenderChildren(context);
-			}
-
-			content.RenderEnd(context);
-
-			RenderFooter(context);
-			RenderAccelCommands(context);
-			RenderContextMenus();
-
-			dialog.RenderEnd(context);
+			var span = new TagBuilder("span", "modal-title-info");
+			span.RenderStart(context);
+			TitleInfo.RenderElement(context, null);
+			span.RenderEnd(context);
 		}
+		var close = new TagBuilder("button", "btnclose");
+		close.MergeAttribute("@click.prevent", "$modalClose(false)");
+		close.SetInnerText("&#x2715;");
+		close.Render(context);
 
-		public override void RenderChildren(RenderContext context, Action<TagBuilder>? onRenderStatic = null)
+		RenderHelp(context);
+
+		header.RenderEnd(context);
+	}
+
+	static void RenderLoadIndicator(RenderContext context)
+	{
+		new TagBuilder("div", "load-indicator")
+			.MergeAttribute("v-show", "$isLoading")
+			.Render(context);
+	}
+
+	protected virtual void RenderFooter(RenderContext context)
+	{
+		if (Buttons.Count == 0)
+			return;
+		var footer = new TagBuilder("div", "modal-footer");
+		footer.RenderStart(context);
+
+		foreach (var b in Buttons)
+			b.RenderElement(context);
+
+		footer.RenderEnd(context);
+	}
+
+	protected Boolean HasHelp => GetBinding(nameof(HelpUrl)) != null || !String.IsNullOrEmpty(HelpUrl);
+
+	protected virtual void RenderHelp(RenderContext context)
+	{
+		if (!HasHelp)
+			return;
+		var ha = new TagBuilder("a", "btn-help");
+		ha.MergeAttribute("rel", "help");
+		ha.MergeAttribute("title", context.Localize("@[Help]"));
+
+		var hbind = GetBinding(nameof(HelpUrl));
+		if (hbind != null)
 		{
-			// static without wrapper
-			foreach (var c in Children)
-				c.RenderElement(context);
+			String hpath = hbind.GetPathFormat(context);
+			ha.MergeAttribute("@click.stop.prevent", $"$showHelp({hpath})");
+			ha.MergeAttribute(":href", $"$helpHref({hpath})");
 		}
-
-
-		void SetSize(TagBuilder dialog)
+		else if (!String.IsNullOrEmpty(HelpUrl))
 		{
-			if ((Size == DialogSize.Default || Size == DialogSize.Medium) && (Width == null))
-				return;
-			var sb = new StringBuilder("{");
-			if (Size == DialogSize.Large)
-				sb.Append("cssClass:'modal-large',");
-			else if (Size == DialogSize.Small)
-				sb.Append("cssClass:'modal-small',");
-			if (Width != null)
-				sb.Append($"width:'{Width.Value}',");
-			if (MinWidth != null)
-				sb.Append($"minWidth:'{MinWidth.Value}',");
-			sb.RemoveTailComma();
-			sb.Append('}');
-			dialog.MergeAttribute("v-modal-width", sb.ToString());
+			ha.MergeAttribute("@click.stop.prevent", $"$showHelp('{HelpUrl}')");
+			ha.MergeAttribute(":href", $"$helpHref('{HelpUrl}')");
 		}
+		ha.RenderStart(context);
+		new TagBuilder("i", "ico ico-help")
+			.Render(context);
+		//context.Writer.Write(context.Localize("@[Help]"));
+		ha.RenderEnd(context);
+	}
 
-		void RenderHeader(RenderContext context)
-		{
-			var header = new TagBuilder("div", "modal-header");
-			if (!Maximize)
-				header.MergeAttribute("v-drag-window", String.Empty);
-			header.RenderStart(context);
-			var hdr = GetBinding(nameof(Title));
-			if ((hdr != null) || (Title != null))
-			{
-				var span = new TagBuilder("span", "modal-title");
-				if (hdr != null)
-					span.MergeAttribute("v-text", hdr.GetPathFormat(context));
-				else if (Title != null)
-					span.SetInnerText(context.LocalizeCheckApostrophe(Title));
-				span.Render(context);
-			}
-			if (TitleInfo != null)
-			{
-				var span = new TagBuilder("span", "modal-title-info");
-				span.RenderStart(context);
-				TitleInfo.RenderElement(context, null);
-				span.RenderEnd(context);
-			}
-			var close = new TagBuilder("button", "btnclose");
-			close.MergeAttribute("@click.prevent", "$modalClose(false)");
-			close.SetInnerText("&#x2715;");
-			close.Render(context);
+	public void RenderSecondPhase(RenderContext context)
+	{
+		if (Children.Count != 1 || Children[0] is not EUSignFrame eusignFrame)
+			throw new XamlException("Invalid dialog for two-phase rendering");
+		eusignFrame.RenderTwoPhaseContent(context);
+	}
 
-			RenderHelp(context);
-
-			header.RenderEnd(context);
-		}
-
-		static void RenderLoadIndicator(RenderContext context)
-		{
-			new TagBuilder("div", "load-indicator")
-				.MergeAttribute("v-show", "$isLoading")
-				.Render(context);
-		}
-
-		protected virtual void RenderFooter(RenderContext context)
-		{
-			if (Buttons.Count == 0)
-				return;
-			var footer = new TagBuilder("div", "modal-footer");
-			footer.RenderStart(context);
-
-			foreach (var b in Buttons)
-				b.RenderElement(context);
-
-			footer.RenderEnd(context);
-		}
-
-		protected Boolean HasHelp => GetBinding(nameof(HelpUrl)) != null || !String.IsNullOrEmpty(HelpUrl);
-
-		protected virtual void RenderHelp(RenderContext context)
-		{
-			if (!HasHelp)
-				return;
-			var ha = new TagBuilder("a", "btn-help");
-			ha.MergeAttribute("rel", "help");
-			ha.MergeAttribute("title", context.Localize("@[Help]"));
-
-			var hbind = GetBinding(nameof(HelpUrl));
-			if (hbind != null)
-			{
-				String hpath = hbind.GetPathFormat(context);
-				ha.MergeAttribute("@click.stop.prevent", $"$showHelp({hpath})");
-				ha.MergeAttribute(":href", $"$helpHref({hpath})");
-			}
-			else if (!String.IsNullOrEmpty(HelpUrl))
-			{
-				ha.MergeAttribute("@click.stop.prevent", $"$showHelp('{HelpUrl}')");
-				ha.MergeAttribute(":href", $"$helpHref('{HelpUrl}')");
-			}
-			ha.RenderStart(context);
-			new TagBuilder("i", "ico ico-help")
-				.Render(context);
-			//context.Writer.Write(context.Localize("@[Help]"));
-			ha.RenderEnd(context);
-		}
-
-		public void RenderSecondPhase(RenderContext context)
-		{
-			if (Children.Count != 1 || Children[0] is not EUSignFrame eusignFrame)
-				throw new XamlException("Invalid dialog for two-phase rendering");
-			eusignFrame.RenderTwoPhaseContent(context);
-		}
-
-		protected override void OnEndInit()
-		{
-			base.OnEndInit();
-			if (Size == DialogSize.Max)
-				Maximize = true;
-		}
+	protected override void OnEndInit()
+	{
+		base.OnEndInit();
+		if (Size == DialogSize.Max)
+			Maximize = true;
 	}
 }
+
