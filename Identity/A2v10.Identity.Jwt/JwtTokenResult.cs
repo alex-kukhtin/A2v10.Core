10f8589b8ca14a92cc81a2139bf70f3ba178a4ed
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-01-23 10:30:16 +0200

    Jwt 8051

--- Identity/A2v10.Identity.Jwt/JwtTokenResult.cs
+++ Identity/A2v10.Identity.Jwt/JwtTokenResult.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2021 Alex Kukhtin. All rights reserved.
+﻿// Copyright © 2021-2022 Alex Kukhtin. All rights reserved.
 
 using System;
 
@@ -6,8 +6,12 @@ namespace A2v10.Identity.Jwt
 {
 #pragma warning disable IDE1006 // Naming Styles
     public record JwtTokenResponse(String accessToken, String refreshToken, Int64 validTo, String user, Boolean success = true );
+    public record JwtTokenError(String message)
+    {
+        public Boolean success => false;
+    }
 #pragma warning restore IDE1006 // Naming Styles
 
 
-	public record JwtTokenResult(DateTime Expires, JwtTokenResponse Response);
+    public record JwtTokenResult(DateTime Expires, JwtTokenResponse Response);
 }
