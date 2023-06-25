0acb649a2a1a3a861ae189eeed5bddc129e04b0e
alex-kukhtin <alex.kukhtin@outlook.com>
2023-06-25 09:44:28 +0300

    tree grid fix

--- ViewEngines/A2v10.ViewEngine.Xaml/Controls/TreeGridColumn.cs
+++ ViewEngines/A2v10.ViewEngine.Xaml/Controls/TreeGridColumn.cs
@@ -76,9 +76,6 @@ public class TreeGridColumn : UiContentElement
 
 		AddAligns(td);
 
-		if (Content is ITableControl)
-			td.AddCssClass("ctrl");
-
 		if (ShowButton)
 			td.AddCssClass("indent");
 		else
