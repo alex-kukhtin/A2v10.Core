7c55c6d6120d5ead852eb086d36879eb03db2adb
alex-kukhtin <alex.kukhtin@outlook.com>
2023-09-10 21:01:55 +0300

    canvas.1

---
+++ ViewEngines/A2v10.ViewEngine.Xaml/Controls/Canvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace A2v10.Xaml;
+
+public class Canvas : UIElementBase
+{
+	public override void RenderElement(RenderContext context, Action<TagBuilder>? onRender = null)
+	{
+		var tag = new TagBuilder("canvas", null, IsInGrid);
+		onRender?.Invoke(tag);
+		MergeAttributes(tag, context);
+		tag.Render(context, TagRenderMode.Normal);
+	}
+}
