3599c4abd2c2f60d6bba672bbdb53b53211a6434
alex-kukhtin <alex.kukhtin@outlook.com>
2025-02-25 13:21:04 +0200

    v 8529.final

--- Platform/A2v10.Web.Assets/wwwroot/scripts/tabbed.js
+++ Platform/A2v10.Web.Assets/wwwroot/scripts/tabbed.js
@@ -185,9 +185,9 @@ app.modules['std:signalR'] = function () {
 	});
 })();
 
-// Copyright © 2023-2024 Oleksandr Kukhtin. All rights reserved.
+// Copyright © 2023-2025 Oleksandr Kukhtin. All rights reserved.
 
-/*20240820-8337*/
+/*20259225-8529*/
 
 /* tabbed:shell.js */
 (function () {
@@ -201,10 +201,25 @@ app.modules['std:signalR'] = function () {
 	const modalComponent = component('std:modal');
 	const toastrComponent = component('std:toastr');
 	const store = component('std:store');
+	const MAX_OPENED_TABS = 10;
 
 	let tabKey = 77;
-
+	
 	const tabUrlKey = tab => `${tab.url}:${tab.key}`;
+
+	function getClosestLi(ev) {
+		if (!ev.target) return null;
+		return ev.target.closest('li');
+	}
+
+	function intersectLines(t, c) {
+		let dx = 0;
+		if (t.r > c.r)
+			dx = c.r - t.l;
+		else if (t.l < c.l)
+			dx = t.r - c.l;
+		return dx > 0;
+	}
 
 	app.components["std:shellPlain"] = Vue.extend({
 		store,
@@ -213,6 +228,7 @@ app.modules['std:signalR'] = function () {
 				tabs: [],
 				closedTabs: [],
 				activeTab: null,
+				maxUsed: 0,
 				modals: [],
 				modalRequeryUrl: '',
 				traceEnabled: log.traceEnabled(),
@@ -228,7 +244,8 @@ app.modules['std:signalR'] = function () {
 				lockRoute: false,
 				requestsCount: 0,
 				contextTabKey: 0,
-				newVersionAvailable: false
+				newVersionAvailable: false,
+				movedTab: null
 			};
 		},
 		components: {
@@ -261,7 +278,7 @@ app.modules['std:signalR'] = function () {
 					let parentUrl = '';
 					if (this.activeTab)
 						parentUrl = this.activeTab.url || '';
-					tab = { title: u1.title, url: u1.url, query: u1.query || '', loaded: true, key: tabKey++, root: null, parentUrl: parentUrl, reload: 0, debug: false };
+					tab = { title: u1.title, url: u1.url, query: u1.query || '', cnt: 0, o: this.tabs.length + 1, loaded: true, key: tabKey++, root: null, parentUrl: parentUrl, reload: 0, debug: false };
 					this.tabs.push(tab);
 					var cti = this.closedTabs.findIndex(t => t.url === u1.url);
 					if (cti >= 0)
@@ -269,8 +286,14 @@ app.modules['std:signalR'] = function () {
 				}
 				tab.loaded = true;
 				this.activeTab = tab;
-				if (this.tabs.length > 10)
-					this.tabs.splice(0, 1);
+				this.useTab(tab);
+				if (this.tabs.length > MAX_OPENED_TABS) {
+					let mt = this.tabs.toSorted((a, b) => b.cnt - a.cnt);
+					if (mt.length) {
+						let ix = this.tabs.indexOf(mt[0]);
+						this.tabs.splice(ix, 1);
+					}
+				}
 				this.storeTabs();
 			},
 			navigateUrl(url) {
@@ -331,6 +354,9 @@ app.modules['std:signalR'] = function () {
 			isHomeActive() {
 				return !this.activeTab;
 			},
+			tabTooltip(tab) {
+				return `${tab.url}`;
+			},
 			fitText(t) {
 				if (!t) return 'untitled';
 				return t.length > 30 ? t.substring(0, 30) + '…' : t;
@@ -348,6 +374,11 @@ app.modules['std:signalR'] = function () {
 			homeSource() {
 				return this.homeLoaded ? '/_home/index/0' : null;
 			},
+			useTab(tab) {
+				this.tabs.filter(t => t != tab).forEach(t => t.cnt += 1);
+				tab.cnt = 0;
+				this.maxUsed = Math.max(...this.tabs.map(t => t.cnt));
+			},
 			selectHome(noStore) {
 				this.homeLoaded = true;
 				this.activeTab = null;
@@ -364,6 +395,8 @@ app.modules['std:signalR'] = function () {
 					return;
 				}
 				tab.loaded = true;
+				if (this.activeTab != tab)
+					this.useTab(tab);
 				this.activeTab = tab;
 				document.title = tab.title;
 				this.replaceState(tab);
@@ -402,7 +435,7 @@ app.modules['std:signalR'] = function () {
 					let rt = this.tabs.splice(ix, 1);
 					if (rt.length) {
 						this.closedTabs.unshift(rt[0]);
-						if (this.closedTabs.length > 10)
+						if (this.closedTabs.length > MAX_OPENED_TABS)
 							this.closedTabs.pop();
 					}
 				});
@@ -413,6 +446,7 @@ app.modules['std:signalR'] = function () {
 					if (this.activeTab !== currentTab)
 						this.selectTab(currentTab, true);
 				}
+				this._resortTabs();
 				this.storeTabs();
 			},
 			removeTab(tabIndex) {
@@ -420,18 +454,21 @@ app.modules['std:signalR'] = function () {
 				let parent = this.tabs.find(t => t.url === currentTab.parentUrl);
 				if (parent)
 					this.selectTab(parent, true);
-				else if (tabIndex > 0)
-					this.selectTab(this.tabs[tabIndex - 1], true);
-				else if (this.tabs.length > 1)
-					this.selectTab(this.tabs[tabIndex + 1], true);
-				else
-					this.selectHome(true);
+				if (this.isTabActive(currentTab)) {
+					if (tabIndex > 0)
+						this.selectTab(this.tabs[tabIndex - 1], true);
+					else if (this.tabs.length > 1)
+						this.selectTab(this.tabs[tabIndex + 1], true);
+					else
+						this.selectHome(true);
+				}
 				let rt = this.tabs.splice(tabIndex, 1);
 				if (rt.length) {
 					this.closedTabs.unshift(rt[0]);
-					if (this.closedTabs.length > 10)
+					if (this.closedTabs.length > MAX_OPENED_TABS)
 						this.closedTabs.pop();
 				}
+				this._resortTabs();
 				this.storeTabs();
 			},
 			clearLocalStorage() {
@@ -446,7 +483,8 @@ app.modules['std:signalR'] = function () {
 					keys.forEach(f => window.localStorage.removeItem(f));
 			},
 			storeTabs() {
-				var mapTab = (t) => { return { title: t.title, url: t.url, query: t.query || '', parentUrl: t.parentUrl }; };
+				this.maxUsed = Math.max(...this.tabs.map(t => t.cnt));
+				var mapTab = (t) => { return { title: t.title, url: t.url, cnt: t.cnt, query: t.query || '', parentUrl: t.parentUrl }; };
 				let ix = this.tabs.indexOf(this.activeTab);
 				let tabs = JSON.stringify({
 					index: ix,
@@ -469,11 +507,11 @@ app.modules['std:signalR'] = function () {
 						let loaded = ix === i;
 						if (loaded)
 							this.navigatingUrl = t.url;
-						this.tabs.push({ title: t.title, url: t.url, query: t.query, loaded, key: tabKey++, root: null, parentUrl: t.parentUrl, reload: 0, debug: false });
+						this.tabs.push({ title: t.title, url: t.url, query: t.query, cnt: t.cnt || 0, o: i + 1, loaded, key: tabKey++, root: null, parentUrl: t.parentUrl, reload: 0, debug: false });
 					}
 					for (let i = 0; i < elems.closedTabs.length; i++) {
 						let t = elems.closedTabs[i];
-						this.closedTabs.push({ title: t.title, url: t.url, query: t.query, loaded: true, key: tabKey++ });
+						this.closedTabs.push({ title: t.title, url: t.url, query: t.query, cnt: 0, loaded: true, key: tabKey++ });
 					}
 					if (ix >= 0 && ix < this.tabs.length)
 						this.activeTab = this.tabs[ix];
@@ -481,6 +519,7 @@ app.modules['std:signalR'] = function () {
 						this.selectHome(true);
 				} catch (err) {
 				}
+				this.maxUsed = Math.max(...this.tabs.map(t => t.cnt));
 			},
 			toggleTabPopup() {
 				eventBus.$emit('closeAllPopups');
@@ -503,7 +542,6 @@ app.modules['std:signalR'] = function () {
 				style.top = (ev.clientY - br.top) + 'px';
 				style.left = lp + 'px';
 				menu.classList.add('show');
-				//console.dir(this.contextTabKey);
 			},
 			popupClose() {
 				let t = this.tabs.find(t => t.key === this.contextTabKey);
@@ -513,6 +551,140 @@ app.modules['std:signalR'] = function () {
 				if (this.closedTabs.length <= 0) return;
 				let t = this.closedTabs[0];
 				this.reopenTab(t);
+			},
+			_resortTabs() {
+				let arr = this.tabs;
+				arr.sort((a, b) => a.o - b.o);
+				for (let i = 0; i < arr.length; i++)
+					arr[i].o = i + 1;
+				this.storeTabs();
+			},
+			offsetLeft(t) {
+				let mt = this.movedTab;
+				if (!mt || mt.tab !== t)
+					return "0";
+				return mt.pos + 'px';
+			},
+			isDragged(t) {
+				let mt = this.movedTab;
+				return mt && mt.tab === t;
+			},
+			willClose(t) {
+				return this.tabs.length >= MAX_OPENED_TABS && t.cnt === this.maxUsed;
+			},
+			pointerDown(ev, t) {
+				let li = getClosestLi(ev)
+				if (!li) return;
+				ev.target.setPointerCapture(ev.pointerId);
+				let cr = li.getBoundingClientRect();
+				this.movedTab = { tab: t, x: ev.x - cr.left, l: cr.left, pos: 0 };
+				this._dragContext = { tabs: [], currentTab: -1, pointTab: -1, left: 0 };
+			},
+			pointerUp(ev, t) {
+				ev.target.releasePointerCapture(ev.pointerId);
+				this.movedTab = null;
+				this._dragContext = null;
+				this._resortTabs();
+			},
+			pointerMove(ev, t) {
+				let mt = this.movedTab
+				if (!mt) return;
+
+				let offset = ev.x - mt.l - mt.x;
+				mt.pos = offset;
+
+				let li = getClosestLi(ev)
+				if (!li)
+					return;
+				let cr = li.getBoundingClientRect();
+				let hw = cr.width / 4;
+
+				if (Math.abs(offset) < hw)
+					return;
+
+				this._createOrderedTabs();
+
+				let testTab = { l: ev.x - mt.x, r: ev.x - mt.x + cr.width };
+
+				if (!this._testOrderedTabs(testTab))
+					return;
+
+				let nl = this._swapOrderedTabs(testTab);
+				if (!nl) return;
+				//console.log('set nl:', nl.l, 'i:', nl.i);
+				mt.l = nl.l;
+				offset = ev.x - mt.l - mt.x;
+				mt.pos = offset;
+			},
+			_createOrderedTabs() {
+				if (!this._dragContext) return;
+				if (this._dragContext.tabs.length) return;
+				if (!this.tabs.length) return;
+
+				let findAttr = (t) => {
+					return this.$refs.tab.find(el => el.getAttribute('tab-key') == t.key);
+				};
+
+				let otabs = this.tabs.map((t, ix) => ({
+					tab: t,
+					ref: findAttr(t), w: 0,
+				}));
+				let hcr = this.$refs.home.getBoundingClientRect();
+				this._dragContext.left = hcr.right;
+				let l = hcr.right;
+				for (let i = 0; i < otabs.length; i++) {
+					let t = otabs[i];
+					if (t.tab === this.movedTab.tab)
+						this._dragContext.currentTab = i;
+					let cr = t.ref.getBoundingClientRect();
+					t.w = cr.width;
+					l += t.w;
+				}
+				this._dragContext.tabs = otabs;
+				return otabs;
+			},
+			_testOrderedTabs(testTab) {
+				if (!this.movedTab) return false;
+				let otabs = this._dragContext.tabs;
+				if (!otabs || !otabs.length) return false;
+				let l = this._dragContext.left;
+				for (let i = 0; i < otabs.length; i++) {
+					let t = otabs[i];
+					if (t.tab === this.movedTab.tab)
+						this._dragContext.currentTab = i;
+					if (intersectLines(testTab, { l, r: l + t.w })) {
+						this._dragContext.pointTab = i;
+					}
+					l += t.w;
+				}
+				return this._dragContext.currentTab != -1 &&
+					this._dragContext.pointTab != -1 &&
+					this._dragContext.currentTab != this._dragContext.pointTab;
+			},
+			_swapOrderedTabs(testTab) {
+				if (!this._dragContext) return;
+				let tabs = this._dragContext.tabs;
+				if (!tabs.length) return;
+				let tab1 = tabs[this._dragContext.currentTab];
+				let tab2 = tabs[this._dragContext.pointTab];
+				if (tab1 === tab2) return;
+				if (tab1.tab.o === tab2.tab.o)
+					return;
+				let tmp = tab1.tab.o;
+				tab1.tab.o = tab2.tab.o;
+				tab2.tab.o = tmp;
+				let l = this._dragContext.left;
+				let neo = tabs.toSorted((a, b) => a.tab.o - b.tab.o);
+				this._dragContext.tabs = neo;
+				for (let i = 0; i < neo.length; i++) {
+					let t = neo[i];
+					if (l >= testTab.l) { 
+						this._dragContext.pointTab = i;
+						return { l, i };
+					}
+					l += t.w;
+				}
+				return { l, i: neo.length }; 
 			},
 			popupCloseOther() {
 				if (!this.contextTabKey) return;
@@ -709,6 +881,8 @@ app.modules['std:signalR'] = function () {
 			},
 			updateModelStack(root) {
 				if (this.__dataStack__.length > 0 && this.__dataStack__[0] === root)
+					return;
+				if (!root || !root.$data)
 					return;
 				this.__dataStack__.splice(0);
 				this.__dataStack__.unshift(root);
