83242a129916bd74f410f57feb788cc9547af736
alex-kukhtin <alex.kukhtin@outlook.com>
2025-06-12 16:26:00 +0300

    InvokeTarget - Signal

--- Platform/A2v10.Services/Commands/InvokeCommandInvokeTarget.cs
+++ Platform/A2v10.Services/Commands/InvokeCommandInvokeTarget.cs
@@ -1,11 +1,11 @@
-﻿// Copyright © 2021-2023 Oleksandr Kukhtin. All rights reserved.
+﻿// Copyright © 2021-2025 Oleksandr Kukhtin. All rights reserved.
 
 using System.Text;
 using System.Threading.Tasks;
 
-using Microsoft.Extensions.DependencyInjection;
-
 using Newtonsoft.Json;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace A2v10.Services;
 
@@ -13,12 +13,15 @@ public class InvokeCommandInvokeTarget : IModelInvokeCommand
 {
 	private readonly IServiceProvider _serviceProvider;
 	private readonly IInvokeEngineProvider _engineProvider;
+	private readonly ISignalSender _signalSender;
 
 	public InvokeCommandInvokeTarget(IServiceProvider serviceProvider)
 	{
 		_serviceProvider = serviceProvider;
 		_engineProvider = _serviceProvider.GetRequiredService<IInvokeEngineProvider>();
-	}
+		_signalSender = _serviceProvider.GetRequiredService<ISignalSender>();
+
+    }
 
 	public async Task<IInvokeResult> ExecuteAsync(IModelCommand command, ExpandoObject parameters)
 	{
@@ -29,10 +32,16 @@ public class InvokeCommandInvokeTarget : IModelInvokeCommand
 			throw new InvalidOperationException($"Invalid target: {command.Target}");
 		var engine = _engineProvider.FindEngine(target[0]) 
 			?? throw new InvalidOperationException($"InvokeTarget '{target[0]}' not found");
-            try
-            {
+        try
+        {
 			var res = await engine.InvokeAsync(target[1], parameters);
-			return new InvokeResult(
+			var signal = res.Eval<List<ExpandoObject>>("Signal");
+			if (signal != null && signal.Count > 0)
+			{
+                foreach (var e in signal)
+                    await _signalSender.SendAsync(SignalResult.FromData(e));
+            }
+            return new InvokeResult(
 				body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(res)),
 				contentType: MimeTypes.Application.Json
 			);
